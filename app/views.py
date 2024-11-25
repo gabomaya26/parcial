e5057f56e8b20b2a6a5e2db48a6fe1174011717b
gabomaya26 <fate142001@gmail.com>
2024-11-24 21:20:56 -0500

    add module profile

--- app/views.py
+++ app/views.py
@@ -1,17 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
-from .serializers import AuthorSerializer, MessageSerializer
+from .serializers import MessageSerializer, AuthorSerializer
 from .models import Author, Message
-
 
 @api_view(["GET"])
 def get_messages(request):
     messages = Message.objects.all().order_by("created_at")
     serializer = MessageSerializer(messages, many=True)
     return Response(serializer.data)
-
 
 @api_view(["POST"])
 def create_message(request):
@@ -30,5 +27,44 @@ def create_message(request):
     if serializer.is_valid():
         serializer.save(author=author)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["PUT"])
+def update_profile_picture(request, author_id):
+    try:
+        author = Author.objects.get(id=author_id)
+    except Author.DoesNotExist:
+        return Response(
+            {
+                "error": "Autor no encontrado",
+            },
+                status = status.HTTP_404_NOT_FOUND
+        )
+    serializer = AuthorSerializer(author, data=request.data, partial=True)
+    
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status.HTTP_200_OK)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET"])
+def get_author_by_username(request, username):
+    try:
+        author, _ = Author.objects.get_or_create(name=username)
+    except Author.DoesNotExist:
+        return Response(
+            {
+                "error": "Autor no encontrado",
+            },
+                status = status.HTTP_404_NOT_FOUND
+        )
+        
+    serializer = AuthorSerializer(author, data=request.data, partial=True)
+    
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status.HTTP_200_OK)
     
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
