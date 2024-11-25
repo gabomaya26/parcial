f079f57df280a7c0eee1c826dfd4264c00a76cc7
gabomaya26 <fate142001@gmail.com>
2024-11-24 21:41:05 -0500

    update app/urls.py

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('messages/', view=views.get_messages, name="get-messages"),
     path('messages/create/', view=views.create_message, name="create-message"),
-    path('authors/<int:author_id>/', views.update_profile_picture, name="update-profile-picture"),
+    path('authors/<int:author_id>/profile_picture/', views.update_profile_picture, name="update-profile-picture"),
     path('authors/<str:username>/', views.get_author_by_username, name='get-author-by-username'),
     
 ]
